20aa0439e7025c8d9af14fba4b9a297b79e6eb03
Nur Ardli Rachmat Saputra <nurardli.saputra@gmail.com>
2025-01-24 17:37:39 +0700

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download the file
+wget https://github.com/Project-InitVerse/ini-miner/releases/download/v1.0.0/iniminer-linux-x64
+
+# Make the file executable
+chmod +x iniminer-linux-x64

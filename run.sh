abbad00ff343dc5e9e40211b953199b8405d8cab
Nur Ardli Rachmat Saputra <nurardli.saputra@gmail.com>
2025-01-25 11:28:19 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 # Infinite loop to run the miner
 while true; do
-    ./iniminer-linux-x64 --pool stratum+tcp://0xa2ECBF27C6DfBDF2fb7B21e71ba2067E74f2080D.Worker001@pool-a.yatespool.com:31588 stratum+tcp://0xa2ECBF27C6DfBDF2fb7B21e71ba2067E74f2080D.Worker002@pool-b.yatespool.com:32488  --cpu-devices 0 1
+    ./iniminer-linux-x64 --pool stratum+tcp://0xa2ECBF27C6DfBDF2fb7B21e71ba2067E74f2080D.Worker001@pool-a.yatespool.com:31588 --cpu-devices 0 1
 done
